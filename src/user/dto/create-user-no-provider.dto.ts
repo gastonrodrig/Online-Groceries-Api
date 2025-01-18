0e2537f402e4f01c1dc4f4133eb1ddd0b237fc99
Gaston <gaston.rodriguez@icloud.com>
2025-01-18 17:53:37 -0500

    user with provider and with no provider dto updated

--- src/user/dto/create-user-no-provider.dto.ts
+++ src/user/dto/create-user-no-provider.dto.ts
@@ -2,12 +2,12 @@ import { IsString, IsNotEmpty, IsEmail, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserNoProviderDto {
-  @ApiProperty({ description: 'Full name of the user', example: 'gastonrodrig' })
+  @ApiProperty({ example: 'gastonrodrig' })
   @IsString()
   @IsNotEmpty()
   username: string;
 
-  @ApiProperty({ description: 'Email of the user', example: 'user@example.com' })
+  @ApiProperty({ example: 'user@example.com' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
--- src/user/dto/create-user-with-provider.dto.ts
+++ src/user/dto/create-user-with-provider.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, IsEmail } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserWithProviderDto {
-  @ApiProperty({ description: 'Email of the user', example: 'user@example.com' })
+  @ApiProperty({ example: 'user@example.com' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
